40870cbddb752e8090b9d9963e01085593f550ed
Albert R. González‑Marín <dsifriend@gmail.com>
2025-11-18 00:07:32 -0400

    Removed old posts and non-reusable content.

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -4,11 +4,11 @@
 	We at The Society for Fancy Cat Feet strive to bring you the cutest,
   fanciest cat feet for your <it>purr</it>usal.
 </p>
-<p>
+<!-- <p>
   This feed is maintained by The Cat Kingdom's Ministry of Culture,
   and curated by its Head Minister
   <a href="https://twitter.com/dsifriend">@dsifriend</a>
   and their executive assistant
   <a href="https://twitter.com/flowergothic">@flowergothic</a>.
-</p>
+</p> -->
 <!-- prettier-ignore-end -->
