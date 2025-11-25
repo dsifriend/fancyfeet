e02fb4d523b3e7227cd2d95561a3a7ef322bc752
Albert R. González‑Marín <dsifriend@gmail.com>
2025-11-25 01:51:14 -0400

    Corrected inbox for email contact

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -2,5 +2,5 @@
 <p>
   Should you ever feel the need to get in contact with The Society,
   our Prime Minister of Feline Affairs may be reached at
-  <a href="mailto:inquiries@albertrg.me">his official eMail adress</a>.
+  <a href="mailto:editorial@albertrg.me">his official eMail adress</a>.
 </p>
